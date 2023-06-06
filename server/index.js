c776ddf0f9a692f77f5d0c3d879362679ffd3e5b
Valiulin.Sh.Sh <valiulin.sh.sh@gmail.com>
2023-06-06 17:18:26 +0300

    добавлено получение файла- путь "/название файла"

--- server/index.js
+++ server/index.js
@@ -6,11 +6,13 @@ const cors = require("cors");
 const router = require("./routes/index");
 const errorHandler = require("./middleware/ErrorHandlingMiddleware");
 const fileUpload = require("express-fileupload");
+const path = require("path");
 const PORT = process.env.PORT;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, "static/DB_img")));
 app.use(fileUpload({}));
 app.use("/api", router);
 
